7584c77550c4eeae9c2874919dd7c870c5cb99ea
Fish <fishagbalo@gmail.com>
2025-02-03 21:14:08 +0800

    Add jQuery integration and enhance user details view with navigation and address details toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "jquery";
+
+$(document).on("turbo:load", function () {
+  $("#address_details").on("change", function () {
+    $(".address-details-div").toggleClass("d-none");
+  });
+});
