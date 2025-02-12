09dd23d8d48aa6867c75af0d4658e06e6e085b5f
Fish <fishagbalo@gmail.com>
2025-02-12 21:55:54 +0800

    Implement employee search functionality and add pagination options for page size

--- app/controllers/employeespaginations_controller.rb
+++ app/controllers/employeespaginations_controller.rb
@@ -5,5 +5,9 @@ class EmployeespaginationsController < ApplicationController
   # @employees = Employee.all
   # @employees = Employee.page(params[:page])
   # @employees = Employee.page(params[:page]).per(5)
+
+  # @employees = Employee.page(params[:page]).per(params[:per_page] || 5)
+
+   @employees = Employee.search(params[:search]).page(params[:page]).per(params[:per_page] || 5)
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,13 @@ class Employee < ApplicationRecord
     has_rich_text :notes
     belongs_to :department
     belongs_to :country
+
+    #it's a way to name a custom database query, composed of ActiveRecord methods
+    scope :search, -> (query) {
+        if query.present?
+            where("firstname LIKE ? OR lastname LIKE ? OR CAST(salary AS TEXT) LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+        else 
+            all
+        end
+    }
 end
