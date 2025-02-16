32180592dbc61ae934c49088b8c9e0f4e1d9019d
Fish <fishagbalo@gmail.com>
2025-02-16 13:57:03 +0800

    Implement transfer creation with account balance updates and error handling; remove action buttons from index view

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -8,7 +8,32 @@ class TransfersController < ApplicationController
   end
 
   def create
-    # FAIL
+    @transfer = Transfer.new(transfer_params)
+
+    Transfer.transaction do
+      if @transfer.save!
+        @transfer.deduct_from_account
+        @transfer.deduct_from_account_balance
+        @transfer.add_to_account
+        redirect_to transfers_path, notice: 'Transfer created successfully!'
+      else
+        flash[:alert] = "Field to create transfer: #{@transfer.error.full_messages}"
+        render :new
+      end
+    end 
+    rescue ActiveRecord::RecordInvalid => e
+      render :new
+      flash[:alert] = "Field to create transfer: #{e.message}"
   end 
 
+  private
+
+  def transfer_params
+    params.require(:transfer).permit(:from_account_id, :to_account_id, :amount)
+  end
+
 end
+
+# 3 Record Colaboration here
+# 1 Insert in transfers DB
+# 2 Updates in Account 2 users 
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,4 +1,23 @@
 class Transfer < ApplicationRecord
   belongs_to :from_account, class_name: 'Account'
   belongs_to :to_account, class_name: 'Account'
+
+  def deduct_from_account
+    if from_account.balance < amount
+      errors.add(:base, 'Insufficient balance in account')
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  def deduct_from_account_balance
+    from_account.balance -= amount
+    from_account.save
+  end
+
+  def add_to_account
+    to_account.balance += amount
+    to_account.save
+    #error.add(:base, 'Unable to add to target customer as thier bank server is busy!')
+    #raise ActiveRecord::Rollback
+  end
 end
