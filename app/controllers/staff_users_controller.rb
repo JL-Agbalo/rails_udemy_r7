99bce1049046d413b27c47b2480ee6d9ddb57038
Fish <fishagbalo@gmail.com>
2025-02-24 22:41:54 +0800

    Enhance staff user management; implement edit and update actions, add destroy functionality, and improve role fetching in StaffUsersController. Update edit view for better user experience.

--- app/controllers/staff_users_controller.rb
+++ app/controllers/staff_users_controller.rb
@@ -1,19 +1,65 @@
+# StaffUsersController manages staff users.
+# Actions:
+# - index: Lists all staff users.
+# - new: Initializes a new staff user.
+# - edit: Prepares an existing staff user for editing.
+# - create: Creates a new staff user.
+#
+# Private Methods:
+# - find_staff_user: Finds a staff user by ID.
+# - fetch_roles: Fetches roles excluding the one with ID 1.
+# - staff_user_params: Permits specific parameters for staff user creation and update.
+
+# Before Action: This file is used to render the form for editing a staff user
 class StaffUsersController < ApplicationController
 before_action :authenticate_admin!
-before_action :fetch_roles, only: [:new]
+before_action :fetch_roles, only: [:new, :edit]
+before_action :find_staff_user, only: [:edit, :update, :destroy]
+
+
   def index
-   @staff_user = Admin.staff_user
+   @staff_users = Admin.staff_user
   end
 
   def new
     @staff_user = Admin.staff_user.new
   end
 
+  def edit
+  end
+
+  def update
+    if(@staff_user.update(staff_user_params))
+      flash[:notice] = "Staff user updated successfully"
+      redirect_to staff_users_path
+    else
+      flash[:notice] = @staff_user.errors.full_messages.join(",")
+      redirect_to edit_staff_user_path(@staff_user)
+    end
+  end
+
+
+  def destroy
+    if @staff_user.destroy
+      flash[:notice] = "Staff user deleted successfully"
+    else
+      flash[:notice] = @staff_user.errors.full_messages.join(", ")
+    end
+    redirect_to staff_users_path
+  end
+
+  def find_staff_user
+    @staff_user = Admin.staff_user.find_by(id: params[:id])
+    unless @staff_user
+      flash[:notice] = "Staff user not found"
+      redirect_to staff_users_path
+    end
+  end
+
   def fetch_roles
     @roles = Role.where.not(id: 1).pluck(:name, :id)
   end
 
-  # TODO LENARD CHECK THE SAVING AND SHOWING OF THE STAFF USER
   def create
     @staff_user = Admin.staff_user.new(staff_user_params)
     if @staff_user.save
