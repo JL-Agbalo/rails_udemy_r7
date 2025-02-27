690d5504cde93f0bb6a21f864b237f873f29ebcc
Fish <fishagbalo@gmail.com>
2025-02-27 22:08:14 +0800

    Implement product management functionality; add Product model, create migration for products table, and establish product policies with authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def admin_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  def pundit_user
+    current_admin
+  end
+
 end
